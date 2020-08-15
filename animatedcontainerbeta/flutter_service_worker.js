362976e5d1c3d3afe4a4591a2f0c939fe92f8d63
Mike Rydstrom <m.rydstrom@gmail.com>
2020-08-15 20:01:34 +0300

    Wrong files before, these are the ones for the AnimatedContainer issue built with Web Beta

--- animatedcontainerbeta/flutter_service_worker.js
+++ animatedcontainerbeta/flutter_service_worker.js
@@ -3,63 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "ba95592990b8a077c8ab484f02d36bb2",
-"assets/assets/fonts/Roboto/Roboto-Black.ttf": "3234b3e853eabe9fc936cd7a133055ad",
-"assets/assets/fonts/Roboto/Roboto-BlackItalic.ttf": "18a576bf7ceb253929ac1f7bdfe6d467",
-"assets/assets/fonts/Roboto/Roboto-Bold.ttf": "7c18188784f21915f42a5b3bc9d91e20",
-"assets/assets/fonts/Roboto/Roboto-BoldItalic.ttf": "9098d3de14ef1cfae7be4f195fe1a174",
-"assets/assets/fonts/Roboto/Roboto-Light.ttf": "c6cdfded4630ba6d9a2dceb70aa4fe0f",
-"assets/assets/fonts/Roboto/Roboto-LightItalic.ttf": "afd48b813752d70b936e73d99b5dca35",
-"assets/assets/fonts/Roboto/Roboto-Medium.ttf": "d52f011be65b281ba8ca1c3f689cf133",
-"assets/assets/fonts/Roboto/Roboto-MediumItalic.ttf": "c1d8fbd29929d048c8d065d1be3bc35d",
-"assets/assets/fonts/Roboto/Roboto-Regular.ttf": "18d44f79b3979ec168862093208c6d7d",
-"assets/assets/fonts/Roboto/Roboto-RegularItalic.ttf": "9da6e8b1803fec548ee0720f620f93f5",
-"assets/assets/fonts/Roboto/Roboto-Thin.ttf": "d1e42f1b6d0c32271f8e68515563a213",
-"assets/assets/fonts/Roboto/Roboto-ThinItalic.ttf": "2cbc2256ef5afbf04c18beeee4478e55",
-"assets/assets/fonts/rubikmono/RubikMonoOne-Regular.ttf": "00c8b3ccb1e038d8c63a419b76a7debe",
-"assets/assets/images/Active%2520options.svg": "0745134a75e3835e64b4b1f830413a34",
-"assets/assets/images/App%2520installation.svg": "91775dd59e4097f3be7ee28e71e0eb75",
-"assets/assets/images/Apps.svg": "549b71f37205d7ba2eb316ca368430b7",
-"assets/assets/images/Building%2520blocks.svg": "71ee150b7eb86c82e9222b67b74cd4e4",
-"assets/assets/images/Choice.svg": "9aa3135acea46c7d7964e89ee8dd1347",
-"assets/assets/images/Dark%2520mode.svg": "cd0e162b95cd9b6130eed4338e665648",
-"assets/assets/images/File%2520sync.svg": "cb8be86399adbe526e658fbd6e6a3334",
-"assets/assets/images/Healthy%2520options.svg": "3e78385e5e98be1f000c0174899ea65e",
-"assets/assets/images/Home%2520screen.svg": "cf549a61f9e7015311d288e2e0d344f2",
-"assets/assets/images/In%2520progress.svg": "f1ad2b47764f8d7fa01eb8be7b07bd4d",
-"assets/assets/images/In%2520sync.svg": "0e06e3ea9fc90a5c005b1c6c5998e099",
-"assets/assets/images/Mobile%2520apps.svg": "9ab42ab85e371b8d9bdececb2cdb9618",
-"assets/assets/images/Mobile%2520devices.svg": "f663df7f57bea6e9b5248690265d1421",
-"assets/assets/images/Mobile%2520feed.svg": "ce23adbc258ad719e368e625a37c61bf",
-"assets/assets/images/Mobile%2520web.svg": "6de6aa3d8e045d6d1ca91006752c5c3b",
-"assets/assets/images/Modern%2520design.svg": "759ed5bf7c531b5eeb3433ad293fc821",
-"assets/assets/images/My%2520notifications.svg": "efd4d3569d9cf7cc4be26f60482c46af",
-"assets/assets/images/Nature%2520on%2520screen.svg": "0256b07845db59084042c687e78e76f3",
-"assets/assets/images/Open%2520source.svg": "8d43ab9744ffc0172d0564b8058ff46e",
-"assets/assets/images/Opened%2520tabs.svg": "c5a285a8afa8a460e2570b682f6221ab",
-"assets/assets/images/Post%2520online.svg": "5c6186af5b661fd081a30aee372b0111",
-"assets/assets/images/Posting%2520photo.svg": "9e4bc0125385ab8f8a45fed2f459d90b",
-"assets/assets/images/Prioritise.svg": "082f884f5b06cd951fd0d7256f84cb00",
-"assets/assets/images/Product%2520teardown.svg": "380535d59f7fc5c39c11767adee84f76",
-"assets/assets/images/Progress%2520overview.svg": "d818b52fb91eeedbf22c493e308d62ac",
-"assets/assets/images/Progressive%2520app.svg": "93b50dae99302df619ea8767dc332d88",
-"assets/assets/images/Questions.svg": "57b8f6fc99798e833c2ad35ed20c4b0a",
-"assets/assets/images/RM400.jpg": "116399354873d9d19b8bc482291dcd9c",
-"assets/assets/images/Set%2520preferences.svg": "164e0e23d0897428bd42ced3b4ed9841",
-"assets/assets/images/Setup%2520wizard.svg": "1d6e1b509ab056cccd45d39abc669b5a",
-"assets/assets/images/Web%2520devices.svg": "282dfc9bcc1f6fdfe54e4abc26bbd3e2",
-"assets/FontManifest.json": "4bdfca58b1e7e93d6cd25daa925f57b1",
-"assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
-"assets/NOTICES": "4ea811e51f7d69892fcc04c8a68fc3af",
+  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/NOTICES": "10f942356dc59234410a78335d191a93",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "6a2ddad1092a0a1c326b6d0e738e682b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "88c64932ee95328b6a1279b43dcdd5b7",
-"/": "88c64932ee95328b6a1279b43dcdd5b7",
-"main.dart.js": "6e4ea518a78d2e113f5fdd6d1817e05e",
-"manifest.json": "683e9781a8276b938dac16f82fe1e433"
+"index.html": "3ddd1a69259a9b6514b10fd5fe757634",
+"/": "3ddd1a69259a9b6514b10fd5fe757634",
+"main.dart.js": "2c77e6a59eefd2befe856d80ed5d1b3d",
+"manifest.json": "a8ecee70a417a720cf1d64edb54d7e59"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -76,8 +31,8 @@ const CORE = [
 self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
-      // Provide a 'reload' param to ensure the latest version is downloaded.
-      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'reload'})));
+      // Provide a no-cache param to ensure the latest version is downloaded.
+      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'no-cache'})));
     })
   );
 });
@@ -160,7 +115,7 @@ self.addEventListener("fetch", (event) => {
         // Either respond with the cached resource, or perform a fetch and
         // lazily populate the cache. Ensure the resources are not cached
         // by the browser for longer than the service worker expects.
-        var modifiedRequest = new Request(event.request, {'cache': 'reload'});
+        var modifiedRequest = new Request(event.request, {'cache': 'no-cache'});
         return response || fetch(modifiedRequest).then((response) => {
           cache.put(event.request, response.clone());
           return response;
