d3985e645e6cc12901c96be7463ff90448734aba
Mike Rydstrom <m.rydstrom@gmail.com>
2020-08-22 08:43:07 +0300

    Moved the FAB location to bottom places due to the FAB bug, chnaged some default settings to better show capabilites on an iPad 2nd attempt with better defaults

--- demoflexfold/flutter_service_worker.js
+++ demoflexfold/flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "88c64932ee95328b6a1279b43dcdd5b7",
 "/": "88c64932ee95328b6a1279b43dcdd5b7",
-"main.dart.js": "dbf03d3f5f08c5128127a8c5e7106455",
+"main.dart.js": "f78dc1b63c7c929fb46665e9367c7c31",
 "manifest.json": "683e9781a8276b938dac16f82fe1e433"
 };
 
