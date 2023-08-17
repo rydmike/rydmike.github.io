075ede86577d205712f349548cf2e6f00c5fbdcb
rydmike <m.rydstrom@gmail.com>
2023-08-17 21:01:13 +0000

    Update from https://github.com/rydmike/flex_color_scheme/commit/7380403c57abef607c3d03751ce4ab1da9f3b937

--- flexcolorscheme/allthemes-latest/main.dart.js
+++ flexcolorscheme/allthemes-latest/main.dart.js
@@ -47814,7 +47814,7 @@ if(i==null)i=r
 h=s.f
 s=s.w
 g=t.p
-A.aM0(A.bW(A.d([A.ad_(f,4,0,new A.ap(r,i,h,s==null?h:s,f,f,f,f,f,!1),f,30,f,f,!0,f,f,j,f,f,f,!0,f,f,30)],g),B.A,B.cE,B.w,f),"\xa9 2020 - 2023\nMike Rydstrom\nBSD 3-Clause License",k,"7.3.0 \nBuild-01",A.d([new A.W(B.alv,A.aka(f,f,B.aW,f,f,!0,f,A.ck(A.d([A.ck(f,f,b,"The "+e.wl(l).CW+" application demonstrates features of the FlexColor\xadScheme Flutter theming package.\n\nTo learn more, check out the package on "),A.b_x(q,"pub.dev",$.aU1()),A.ck(f,f,b,". It also includes the source code of this application.\n\n"),A.ck(f,f,c,"Built with Flutter stable 3.13.0 (canvaskit), using FlexColor\xadScheme 7.3.0\nMedia size (w:"+B.c.aa(p.a,0)+", h:"+B.c.aa(p.b,0)+")\n\n")],t.Ne),f,f,f),B.ah,f,f,1,B.ab),f)],g),e,!0)},
+A.aM0(A.bW(A.d([A.ad_(f,4,0,new A.ap(r,i,h,s==null?h:s,f,f,f,f,f,!1),f,30,f,f,!0,f,f,j,f,f,f,!0,f,f,30)],g),B.A,B.cE,B.w,f),"\xa9 2020 - 2023\nMike Rydstrom\nBSD 3-Clause License",k,"7.3.1 \nBuild-01",A.d([new A.W(B.alv,A.aka(f,f,B.aW,f,f,!0,f,A.ck(A.d([A.ck(f,f,b,"The "+e.wl(l).CW+" application demonstrates features of the FlexColor\xadScheme Flutter theming package.\n\nTo learn more, check out the package on "),A.b_x(q,"pub.dev",$.aU1()),A.ck(f,f,b,". It also includes the source code of this application.\n\n"),A.ck(f,f,c,"Built with Flutter stable 3.13.0 (canvaskit), using FlexColor\xadScheme 7.3.1\nMedia size (w:"+B.c.aa(p.a,0)+", h:"+B.c.aa(p.b,0)+")\n\n")],t.Ne),f,f,f),B.ah,f,f,1,B.ab),f)],g),e,!0)},
 $S:0}
 A.Ay.prototype={
 U(){return new A.VH(B.i)}}
