6b53318721a6e4426019806c3fb4e5e8eca932dc
rydmike <m.rydstrom@gmail.com>
2024-06-28 19:13:53 +0000

    Update from https://github.com/rydmike/flex_color_picker/commit/c87bceb1e3f3a43fda80d02c7f68d3a0830cfd0b

--- flexcolorpicker/flutter_bootstrap.js
+++ flexcolorpicker/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e31a","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5ed4","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "821363371"
+    serviceWorkerVersion: "4067604447"
   }
 });
