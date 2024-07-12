7343ef85cf0d76272adb61adb9666b67e02e37fd
rydmike <m.rydstrom@gmail.com>
2024-07-12 19:19:11 +0000

    Update from https://github.com/rydmike/flex_seed_scheme/commit/31673557a4699fde693f21977a166a2a4bcb2f6e

--- flexseedscheme/demo-v3/flutter_bootstrap.js
+++ flexseedscheme/demo-v3/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1915724753"
+    serviceWorkerVersion: "3408752633"
   }
 });
