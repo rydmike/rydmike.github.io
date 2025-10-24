2c0524b859c8033dd6d11d29572994100be86eef
rydmike <m.rydstrom@gmail.com>
2025-10-24 13:25:52 +0000

    Update from https://github.com/rydmike/flex_color_scheme/commit/019a1b9ffd03de4b47c7513d6c53bad5ea447218

--- flexcolorscheme/fourthemes-latest/flutter_bootstrap.js
+++ flexcolorscheme/fourthemes-latest/flutter_bootstrap.js
@@ -34,10 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f7f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7ca","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "143849702"
+    serviceWorkerVersion: "209976877"
   }
 });
